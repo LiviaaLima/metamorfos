495d69d5bd73f08aa711056797d296ad86452586
LiviaaLima <liviaalmeidadelima08@gmail.com>
2025-04-16 20:40:19 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -2,3 +2,28 @@ function toggleMenu() {
     const menu = document.getElementById('mobileMenu');
     menu.classList.toggle('active');
   }
+
+
+  /*Acessibilidade*/
+  
+  function togglePanel() {
+    const panel = document.getElementById("accessibility-options");
+    panel.classList.toggle("hidden");
+  }
+  
+  function toggleHighContrast() {
+    document.body.classList.toggle("high-contrast");
+  }
+  
+  function toggleDyslexicFont() {
+    document.body.classList.toggle("dyslexic-font");
+  }
+  
+  function toggleColorBlindMode() {
+    document.body.classList.toggle("colorblind-mode");
+  }
+  
+  function adjustFontSize(size) {
+    document.documentElement.style.fontSize = size + "px";
+  }
+  
